d26edd6a196daa22638cd057a2adc7e26ce67d8d
Karan Diwan <karandiwan46@gmail.com>
2026-01-21 19:10:49 +0530

    Monostable-Clock in Verilog

---
+++ CLOCK/CLOCK-Verilog/Monostable/M_clk_tb.v
@@ -0,0 +1,48 @@
+`include "M_clk.v"
+
+module testbench;
+
+  reg push;
+  wire clk;
+
+  monostable_clk dut(
+    .push(push),
+    .clk(clk)
+  );
+
+  initial begin
+    
+    $monitor("t = %0t  |  push = %b  | clk = %b ", $time, push, clk);
+    $dumpfile("Sim.vcd");
+    $dumpvars(0, testbench);
+
+    push = 0;
+    #10;
+
+    push = 1;
+    #10;
+
+    push = 0;
+    #10;
+
+    push = 0;
+    #10;
+
+    push = 0;
+    #10;
+
+    push = 0;
+    #10;
+
+    push = 0;
+    #10;
+    
+    push = 1;
+    #10;
+
+    $display("Simulation Complete!");
+    $finish;
+
+  end
+
+endmodule
